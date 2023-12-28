05d4a838efcbc5991a689ba8097a083f26bde041
juanp-ctrl <juanpa11121@hotmail.com>
2023-12-28 13:15:21 -0500

    Hamburguer menu changed and improved :)

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -55,36 +55,6 @@ export default component$(() => {
                 </div>
 
                 <div class={[styles.menu_bg, isActive.value ? styles.change_bg : '']} id="menu-bg"></div>
-
-
-                    {/* <div class={styles.menu}>
-                        <div class={styles.options}>
-                            <div class={[styles.option, styles.selected]}>Inicio</div>
-                            <svg class={styles.option_white_row} xmlns="http://www.w3.org/2000/svg" width="120" height="2" viewBox="0 0 120 2" fill="none">
-                                <path d="M0 1H120" stroke="white"/>
-                            </svg>
-                            <div class={styles.option}>Galeria</div>
-                            <svg class={styles.option_white_row} xmlns="http://www.w3.org/2000/svg" width="120" height="2" viewBox="0 0 120 2" fill="none">
-                                <path d="M0 1H120" stroke="white"/>
-                            </svg>
-                            <div class={styles.option}>CV</div>
-                            <svg class={styles.option_white_row} xmlns="http://www.w3.org/2000/svg" width="120" height="2" viewBox="0 0 120 2" fill="none">
-                                <path d="M0 1H120" stroke="white"/>
-                            </svg>
-                        </div>
-                    </div>
-                    <svg class={styles.x} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 220 200">
-                        <g class={styles.top_bars} stroke-width="2">
-                            <path class={[styles.bar, styles.bar1]} d="M 187,10 H 207" />
-                            <path class={[styles.bar, styles.bar2]} d="M 187,17 H 207" />
-                        </g>
-                    </svg>
-                    <div class={styles.menu_click_area} onClick$={() => {
-                        isActive.value = !isActive.value;
-                    }
-                    }>
-                        
-                    </div> */}
             </div>
         </div>
     );
